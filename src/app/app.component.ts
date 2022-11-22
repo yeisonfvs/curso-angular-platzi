3b55f37c778107b500da08f4036b348e504af263
SamirCortesSoho <edwin@soho.com.uy>
2022-11-22 18:03:04 -0500

    primeros apuntes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+ public name= 'yeison';
+ public age = 50;
+ imagen = "https://picsum.photos/100"
+ btnDisabled = true;
+
+ person = {
+  name: "fabian",
+  age: 20,
+  imagen: "https://picsum.photos/100"
+
+ }
+
+ toggleButton() {
+  this.btnDisabled = !this.btnDisabled;
+ }
+
+ incrementage() {
+  this.person.age += 1;
+ }
+
+ onScroll(event: Event) {
+   const element = event.target as HTMLElement;
+   console.log(element.scrollTop);
+ }
+
+ changeName(event: Event) {
+  const element = event.target as HTMLInputElement;
+  this.person.name = element.value;
+
+ }
+ 
 }
+
+
---
+++ src/app/recap.ts
@@ -0,0 +1,16 @@
+const username: string = 'yeison';
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+sum(1,3);
+
+class person {
+   
+
+   constructor(public age: number, public lastName: string){
+    
+   }
+
+}
+const yei = new person( 26, 'valencia');
+yei.age;
